8eee0efb4fa0ead7f748ae54a5df349f0177c45c
Wassam <wassam@wassam.com>
2025-03-03 13:45:41 -0500

    refactor handleChatCommand to enhance context handling and improve output formatting

--- src/commands/chatCommand.ts
+++ src/commands/chatCommand.ts
@@ -1,45 +1,78 @@
+import hljs from "highlight.js";
+import { markedHighlight } from "marked-highlight";
 import ollama from "ollama";
+import * as path from "path";
 import * as vscode from "vscode";
+const { Marked } = require("marked");
+
+const marked = new Marked(
+  markedHighlight({
+    emptyLangClass: "hljs",
+    langPrefix: "hljs language-",
+    highlight(code, lang, info) {
+      const language = hljs.getLanguage(lang) ? lang : "plaintext";
+      return hljs.highlight(code, { language }).value;
+    },
+  })
+);
+
 import {
+  COMMAND,
   DEFAULT_MODEL,
   EXTENSION_NAME,
   EXTENSION_PARAM_LOCALMODEL,
-} from "../types";
-import { getActiveDocumentFileName } from "../utils/editorUtils";
+} from "../models/constants";
+import { FileInfo } from "../models/interfaces";
 
 export async function handleChatCommand(
   userPrompt: string,
+  fileList: FileInfo[],
   panel: vscode.WebviewPanel,
-  activeEditor: vscode.TextEditor | undefined
+  useActiveEditor: boolean = true,
+  activeEditor?: vscode.TextEditor
 ) {
   let responseText = "";
+  userPrompt += "\nContext for this prompt: \n";
+
+  let contextMessage = "";
 
   if (activeEditor) {
-    const document = activeEditor.document;
-    let content = "";
-    let contextMessage = "";
-
     if (!activeEditor.selection.isEmpty) {
-      content = document.getText(activeEditor.selection);
-      contextMessage = "Using Selected Text as Context.";
-    } else {
-      const fileName = getActiveDocumentFileName(activeEditor);
-      content = document.getText();
-      contextMessage = `Using Active File as Context: ${fileName}`;
+      userPrompt += activeEditor.document.getText(activeEditor.selection);
+      contextMessage = "Using Selected Text as Context.\n";
     }
-
-    panel.webview.postMessage({
-      command: "updateContext",
-      text: contextMessage,
-    });
-
-    userPrompt += "\n" + content;
+    if (useActiveEditor) {
+      const fullPathFileName = activeEditor.document.fileName;
+      const fileName = path.basename(activeEditor.document.fileName);
+      userPrompt += `Content of file ${fullPathFileName}: ${activeEditor.document.getText()}`;
+      contextMessage = `Using Active File as Context: ${fileName}\n`;
+    }
   }
+  if (fileList) {
+    for (const file of fileList) {
+      const fileUri = vscode.Uri.file(file.path);
+      const fileData = await vscode.workspace.fs.readFile(fileUri);
+      const content = Buffer.from(fileData).toString("utf8");
+      userPrompt += `Content of file ${file.path}:\n ${content}\n`;
+    }
+    if (fileList.length > 0) {
+      contextMessage += `Using Selected Files as Context: ${fileList
+        .map((file) => file.name)
+        .join(", ")}`;
+    }
+  }
+  //update extension panel to reveal context being used
+  panel.webview.postMessage({
+    command: COMMAND.UPDATE_CONTEXT,
+    data: contextMessage,
+  });
 
   try {
+    // Get the local model from the extension configuration.
     const config = vscode.workspace.getConfiguration(EXTENSION_NAME);
     const localModel =
       config.get<string>(EXTENSION_PARAM_LOCALMODEL) ?? DEFAULT_MODEL;
+    // Call the chat method from the Ollama API and stream the response to the webview panel.
     const streamResponse = await ollama.chat({
       model: localModel,
       messages: [{ role: "user", content: userPrompt }],
@@ -50,15 +83,15 @@ export async function handleChatCommand(
       responseText += part.message.content;
 
       panel.webview.postMessage({
-        command: "chatResponse",
-        text: responseText,
+        command: COMMAND.CHAT_RESPONSE,
+        data: marked.parse(responseText),
       });
     }
   } catch (error) {
     responseText = "Sorry, I am not able to respond to that.";
     panel.webview.postMessage({
-      command: "chatResponse",
-      text: `${responseText} Error: ${String(error)}`,
+      command: COMMAND.ERROR,
+      data: `${responseText} Error: ${String(error)}`,
     });
   }
 }
